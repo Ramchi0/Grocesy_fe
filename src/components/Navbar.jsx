dda975620643d592f81b84834ad9f45fb4f3d660
Ramchi0 <ramchiramchi16@gmail.com>
2025-08-01 20:27:10 +0530

    Code pushed2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,68 +1,24 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
 import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
 import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
-import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto',
-    },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '20ch',
-        },
-    },
-}));
-
-export default function Navbar({carditems,setIsCardOpen}) {
+export default function Navbar({ carditems, setIsCardOpen }) {
     const [profile, setProfile] = useState("")
     const id = localStorage.getItem("user")
-    var nav=useNavigate()
+    var nav = useNavigate()
     useEffect(() => {
 
         axios.get(`https://grocesy.onrender.com/getid/${id}`)
@@ -94,9 +50,9 @@ export default function Navbar({carditems,setIsCardOpen}) {
         setAnchorEl(null);
         handleMobileMenuClose();
         localStorage.clear();
-        
-       nav("/")
-       window.location.reload()
+
+        nav("/")
+        window.location.reload()
     }
 
     const handleMobileMenuOpen = (event) => {
@@ -143,24 +99,12 @@ export default function Navbar({carditems,setIsCardOpen}) {
             onClose={handleMobileMenuClose}
         >
             <MenuItem>
-                <IconButton size="large" aria-label="show 4 new mails" color="inherit" onClick={()=> setIsCardOpen(true)}>
+                <IconButton size="large" aria-label="show 4 new mails" color="inherit" onClick={() => setIsCardOpen(true)}>
                     <Badge badgeContent={carditems.length} color="error">
                         <MailIcon />
                     </Badge>
                 </IconButton>
-                <p>Messages</p>
-            </MenuItem>
-            <MenuItem>
-                <IconButton
-                    size="large"
-                    aria-label="show 17 new notifications"
-                    color="inherit"
-                >
-                    <Badge badgeContent={17} color="error">
-                        <NotificationsIcon />
-                    </Badge>
-                </IconButton>
-                <p>Notifications</p>
+                <p>cart</p>
             </MenuItem>
             <MenuItem onClick={handleProfileMenuOpen}>
                 <IconButton
@@ -186,22 +130,20 @@ export default function Navbar({carditems,setIsCardOpen}) {
                         variant="h6"
                         noWrap
                         component="div"
-                        sx={{ display: { xs: 'none', sm: 'block' } }}
+                        sx={{ display: { xs: 'block' }, fontWeight: 'bold', color: 'greenyellow' }}
                     >
-                        <b style={{ fontSize: "40px", color: "greenyellow" }}>Grocesy</b><small>app</small>
+                        <Box sx={{ display: { xs: 'none', sm: 'inline' }, fontSize: { sm: '30px', md: '40px' } }}>
+                            Grocesy<small style={{ fontSize: "16px", color: "white" }}>app</small>
+                        </Box>
+                        <Box sx={{ display: { xs: 'inline', sm: 'none' }, fontSize: '24px' }}>
+                            G<small style={{ fontSize: "16px", color: "white" }}>app</small>
+                        </Box>
                     </Typography>
-                    <Search>
-                        <SearchIconWrapper>
-                            <SearchIcon />
-                        </SearchIconWrapper>
-                        <StyledInputBase
-                            placeholder="Search…"
-                            inputProps={{ 'aria-label': 'search' }}
-                        />
-                    </Search>
+
+
                     <Box sx={{ flexGrow: 1 }} />
                     <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                        <IconButton size="large" aria-label="show 4 new mails" color="inherit"onClick={()=> setIsCardOpen(true)}>
+                        <IconButton size="large" aria-label="show 4 new mails" color="inherit" onClick={() => setIsCardOpen(true)}>
                             <Badge badgeContent={carditems.length} color="error">
                                 <ShoppingCartCheckoutIcon />
                             </Badge>
